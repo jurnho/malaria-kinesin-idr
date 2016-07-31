b3841feec3f59ab1580d064455f7b60c74ef15be
Jurn Ho <jurn@magicmonster.com>
2016-07-31 01:56:52 +0100

    move predictors into functions

--- src/score.R
+++ src/score.R
@@ -157,108 +157,51 @@ for (p in predictors) {
 
 write.csv(disprot_predictor_scores,file='disprot_predictor_scores.csv')
 
-
 # only keep proteins with a PDB, these are more simliar to kinesins
-disprot=disprot[disprot$pdb.id!='',]
-protein_ids = unique(disprot$protein_id)
-# randomly select 95% for training
+pdb_only=disprot_predictor_scores[disprot_predictor_scores$pdb.id!='',]
+# disordered residue count
+sum(pdb_only$disordered=='Y')
+# ordered residue count
+sum(pdb_only$disordered=='N')
+
+protein_ids = unique(pdb_only$protein_id)
+# randomly select 90% of proteins for training, and 10% for testing
 buckets = sample(2, length(protein_ids), replace=TRUE, prob=c(0.90, 0.10))
 protein_buckets = cbind(protein_ids, buckets)
 colnames(protein_buckets) = c('protein_id','bucket')
-nn2 = merge(disprot,protein_buckets,by=c('protein_id'))
-library(class)
-# randomly select 95% for training
 
-#bucket = sample(2, nrow(disprot), replace=TRUE, prob=c(0.95, 0.05))
+# run knn on the disprot dataset
+knn_disprot = function(pdb_only, protein_buckets, k=3) {
+  knn_data_set = merge(pdb_only,protein_buckets,by=c('protein_id'))
+  library(class)
+  knn_training = knn_data_set[knn_data_set$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  knn_training.labels = knn_data_set[knn_data_set$bucket==1,c('disordered')]
+  knn_testing = knn_data_set[knn_data_set$bucket==2,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  knn_testing.labels = knn_data_set[knn_data_set$bucket==2,c('disordered')]
+  knn_result = knn(train=knn_training, test=knn_testing, cl=knn_training.labels, k)
 
-#ones = round(nrow(disprot) * 0.95)
-#twos = nrow(disprot) - ones
-#bucket = c(rep(1, ones), rep(2, twos))
+  true_positive = sum((knn_result=='Y') & (knn_testing.labels =='Y'))
+  # false negative
+  false_negative = sum((knn_result=='N') & (knn_testing.labels =='Y'))
+  # false positive
+  false_positive = sum((knn_result =='Y') & (knn_testing.labels =='N'))
+  # true negative
+  true_negative = sum((knn_result =='N') & (knn_testing.labels =='N'))
 
-#training_disordered = nn2[nn2$bucket==1 & nn2$disordered=='Y',]
-#training_ordered = nn2[nn2$bucket==1 & nn2$disordered=='N',]
-# assume that we have more ordered than disordered, take a subset of ordered to match the size of disordered
-#training_ordered2 = training_ordered[sample(nrow(training_ordered), nrow(training_disordered)),]
-#training2=rbind(training_disordered, training_ordered2)
-#training = training2[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-#training.labels = training2[,c('disordered')]
-training = nn2[nn2$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-training.labels = nn2[nn2$bucket==1,c('disordered')]
-testing = nn2[nn2$bucket==2,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-testing.labels = nn2[nn2$bucket==2,c('disordered')]
-knn_result = knn(train=training, test=testing, cl=training.labels, k=3)
-## write out test results data
-testing_df = nn2[nn2$bucket==2,]
-testing_df = testing_df[order(testing_df["protein_id"], testing_df["position"]),]
-# sort it
-knn_results = cbind(testing_df, knn_result)
-knn_results_sorted = knn_results[order(knn_results["protein_id"], knn_results["position"]),]
-write.csv(knn_results_sorted, file = "knn_results.csv")
-
-true_positive = sum((knn_result=='Y') & (testing.labels =='Y'))
-# false negative
-false_negative = sum((knn_result=='N') & (testing.labels =='Y'))
-# false positive
-false_positive = sum((knn_result =='Y') & (testing.labels =='N'))
-# true negative
-true_negative = sum((knn_result =='N') & (testing.labels =='N'))
-
-
-knn_sensitivity = true_positive / (true_positive + false_negative)
-knn_specificity = true_negative / (true_negative + false_positive)
-knn_accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
-knn_sensitivity
-knn_specificity
-knn_accuracy
-
-
-
-write.csv(nn2, file = "nn2.csv")
-
-plasmodb_refs = Sys.glob('plasmodb-ref/*csv')
-plasmodb_ref_dfs = lapply(plasmodb_refs, function(plasmo) {
-    read.csv(plasmo, stringsAsFactors=FALSE)
-})
-# https://stat.ethz.ch/pipermail/r-help/2010-September/252046.html
-plasmodb_ref = do.call("rbind", plasmodb_ref_dfs)
-write.csv(plasmodb_ref, file = "plasmodb_ref.csv")
-# get predictions
-plasmo_prediction_files = Sys.glob('plasmodb/*.csv')
-plasmo_prediction_dfs = lapply(plasmo_prediction_files, function(plasmo_prediction_file) {
-    read.csv(plasmo_prediction_file, stringsAsFactors=FALSE)
-})
-# https://stat.ethz.ch/pipermail/r-help/2010-September/252046.html
-plasmo_prediction_results = do.call("rbind", plasmo_prediction_dfs)
-write.csv(plasmo_prediction_results, file = "plasmo_prediction_results.csv")
-plasmodb = plasmodb_ref
-for (p in predictors) {
-    scores = plasmo_prediction_results[plasmo_prediction_results$predictor==p,c('protein_id', 'position','score')]
-    plasmodb = merge(plasmodb,scores,by=c('protein_id','position'))
-    new_column_names = c(head(colnames(plasmodb),-1), p)
-    colnames(plasmodb) = new_column_names
+  knn_sensitivity = true_positive / (true_positive + false_negative)
+  knn_specificity = true_negative / (true_negative + false_positive)
+  knn_accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
+  result = data.frame(k=k,
+    knn_sensitivity=round(knn_sensitivity, 3),
+    knn_specificity=round(knn_specificity, 3),
+    knn_accuracy=round(knn_accuracy,3)
+  )
+  return(result)
 }
-write.csv(plasmodb, file = "plasmodb.csv")
-
-plasmo_knn.training = disprot[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-plasmo_knn.training_labels = disprot[,c('disordered')]
-plasmo_knn.testing = plasmodb[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-
-plasmo_knn.results = knn(train=plasmo_knn.training, test=plasmo_knn.testing, cl=plasmo_knn.training_labels, k=3)
-
-j = cbind(plasmodb, plasmo_knn.results)
-j = j[order(j["protein_id"], j["position"]),]
-write.csv(j, file = "j.csv")
-# with equal
-plasmo_knn.training = training
-plasmo_knn.training_labels = training.labels
-plasmo_knn.testing = plasmodb[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-plasmo_knn.testing = plasmodb[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-
-plasmo_knn.results = knn(train=plasmo_knn.training, test=plasmo_knn.testing, cl=plasmo_knn.training_labels, k=3)
-
-j = cbind(plasmodb, plasmo_knn.results)
-j = j[order(j["protein_id"], j["position"]),]
-write.csv(j, file = "j2.csv")
+knn_disprot(pdb_only, protein_buckets, 3)
+knn_disprot(pdb_only, protein_buckets, 5)
+knn_disprot(pdb_only, protein_buckets, 7)
+knn_disprot(pdb_only, protein_buckets, 9)
 
 y_score = function(disordered_YN) {
     if (disordered_YN == 'Y') {
@@ -273,70 +216,85 @@ to_disorder_YN = function(lm_score) {
     }
     return ('N')
 }
-# linear regression fit
-lm_y = sapply(disprot$disordered, y_score)
-lm_y = as.vector(lm_y)
-str(disprot$disordered)
-str(lm_y)
-head(disprot$disordered)
-head(lm_y)
-disprot_lm = cbind(nn2, lm_y)
-# create lm_data
-lm_data = disprot_lm[disprot_lm$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short','lm_y')]
-# get a fit for lm_y based on prediction scores
-lm_fit = lm(lm_y ~ ., data = lm_data)
-# prediction
-lm_test_data = disprot_lm[disprot_lm$bucket==2,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-lm_test_labels = disprot_lm[disprot_lm$bucket==2,c('disordered')]
-lm_predictions = predict(lm_fit, lm_test_data)
-lm_predictions_YN = sapply(lm_predictions,to_disorder_YN)
-lm_predictions_YN = as.vector(lm_predictions_YN)
-lm_results = disprot_lm[disprot_lm$bucket==2,]
-lm_results = cbind(lm_results, lm_predictions, lm_predictions_YN)
-lm_results = lm_results[order(lm_results["protein_id"], lm_results["position"]),]
-write.csv(lm_results, file = "lm_results.csv")
 
-predictions_sorted = cbind(knn_results_sorted, lm_results$lm_predictions_YN)
-# write.csv(predictions_sorted, file = "predictions_sorted.csv")
-# append lm results
+# linear regression
+lm_disprot = function(pdb_only, protein_buckets) {
+  lm_y = sapply(pdb_only$disordered, y_score)
+  lm_y = as.vector(lm_y)
+  # add a new y_score column as 1 or disorder, 0 for ordered.
+  disprot_lm = cbind(pdb_only, lm_y)
+  lm_data_set = merge(disprot_lm,protein_buckets,by=c('protein_id'))
+  lm_data = lm_data_set[lm_data_set$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short','lm_y')]
+  # use all of the above
+  lm_fit = lm(lm_y ~ ., data = lm_data)
+  message(coef(lm_fit))
+  lm_test_data = lm_data_set[lm_data_set$bucket==2,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  lm_test_labels = lm_data_set[lm_data_set$bucket==2,c('disordered')]
+  lm_predictions = predict(lm_fit, lm_test_data)
+  lm_predictions_YN = sapply(lm_predictions,to_disorder_YN)
+  lm_predictions_YN = as.vector(lm_predictions_YN)
+  lm_results = lm_data_set[lm_data_set$bucket==2,]
+  lm_results = cbind(lm_results, lm_predictions, lm_predictions_YN)
+  lm_results = lm_results[order(lm_results["protein_id"], lm_results["position"]),]
+  write.csv(lm_results, file = "lm_results.csv")
+  true_positive = sum((lm_predictions_YN=='Y') & (lm_test_labels =='Y'))
+  # false negative
+  false_negative = sum((lm_predictions_YN=='N') & (lm_test_labels =='Y'))
+  # false positive
+  false_positive = sum((lm_predictions_YN =='Y') & (lm_test_labels =='N'))
+  # true negative
+  true_negative = sum((lm_predictions_YN =='N') & (lm_test_labels =='N'))
+  lm_sensitivity = true_positive / (true_positive + false_negative)
+  lm_specificity = true_negative / (true_negative + false_positive)
+  lm_accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
+  result = data.frame(
+    lm_sensitivity = round(lm_sensitivity, 3),
+    lm_specificity = round(lm_specificity, 3),
+    lm_accuracy = round(lm_accuracy, 3)
+  )
+  return (result)
+}
+lm_disprot(pdb_only, protein_buckets)
 
-true_positive = sum((lm_predictions_YN=='Y') & (lm_test_labels =='Y'))
-# false negative
-false_negative = sum((lm_predictions_YN=='N') & (lm_test_labels =='Y'))
-# false positive
-false_positive = sum((lm_predictions_YN =='Y') & (lm_test_labels =='N'))
-# true negative
-true_negative = sum((lm_predictions_YN =='N') & (lm_test_labels =='N'))
+svm_disprot = function(pdb_only, protein_buckets, yes_weight=4, num_samples=5000) {
+  library("e1071")
+  svm_data_set= merge(pdb_only,protein_buckets,by=c('protein_id'))
+  svm_train.all = svm_data_set[svm_data_set$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short','disordered')]
+  training_subset = sample(nrow(svm_train.all), num_samples)
+  svm_train.subset = svm_train.all[training_subset,]
+  svm_train.data = svm_train.subset[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  svm_train.labels = as.factor(svm_train.subset[,c('disordered')])
+
+  svm_model = svm(x=svm_train.data, y=svm_train.labels, probability = TRUE, class.weights=c('Y'=yes_weight, 'N'=1))
+  svm_test_all = svm_data_set[svm_data_set$bucket==2,c('protein_id','position','disordered','DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  svm_test_all = svm_test_all[order(svm_test_all["protein_id"], svm_test_all["position"]),]
+  svm_test_data = svm_test_all[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
+  svm_test_labels = svm_test_all[,c('disordered')]
+  # svm_prediction = predict(svm_model, svm_test_data, decision.values = TRUE, probability = TRUE)
+  svm_prediction = predict(svm_model, svm_test_data)
+  svm_prediction_YN = as.vector(svm_prediction)
+  svm_results = cbind(svm_test_all, svm_prediction_YN)
+
+  true_positive = sum((svm_prediction_YN=='Y') & (svm_test_labels =='Y'))
+  false_negative = sum((svm_prediction_YN=='N') & (svm_test_labels =='Y'))
+  false_positive = sum((svm_prediction_YN =='Y') & (svm_test_labels =='N'))
+  true_negative = sum((svm_prediction_YN =='N') & (svm_test_labels =='N'))
+
+  svm_sensitivity = true_positive / (true_positive + false_negative)
+  svm_specificity = true_negative / (true_negative + false_positive)
+  svm_accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
+
+  result = c('svm_sensitivity'=svm_sensitivity, 'svm_specificity'=svm_specificity, 'svm_accuracy'=svm_accuracy)
+  return (result)
+}
+svm_disprot(pdb_only, protein_buckets)
 
 
-lm_sensitivity = true_positive / (true_positive + false_negative)
-lm_specificity = true_negative / (true_negative + false_positive)
-lm_accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
-lm_sensitivity
-lm_specificity
-lm_accuracy
 
-library("e1071")
-yes_weight=4
-num_samples=5000
 
-svm_train.all = nn2[nn2$bucket==1,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short','disordered')]
-training_subset = sample(nrow(svm_train.all), num_samples)
-svm_train.subset = svm_train.all[training_subset,]
-svm_train.data = svm_train.subset[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-svm_train.labels = as.factor(svm_train.subset[,c('disordered')])
 
-svm_model = svm(x=svm_train.data, y=svm_train.labels, probability = TRUE, class.weights=c('Y'=yes_weight, 'N'=1))
 # nrow(svm_train.data.all)
 
-svm_test_all = nn2[nn2$bucket==2,c('protein_id','position','disordered','DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-svm_test_all = svm_test_all[order(svm_test_all["protein_id"], svm_test_all["position"]),]
-svm_test_data = svm_test_all[,c('DISEMBL_COILS','DISEMBL_REM465','DISEMBL_HOTLOOPS','DISOPRED','iupred_long','iupred_short')]
-svm_test_labels = svm_test_all[,c('disordered')]
-# svm_prediction = predict(svm_model, svm_test_data, decision.values = TRUE, probability = TRUE)
-svm_prediction = predict(svm_model, svm_test_data)
-svm_prediction_YN = as.vector(svm_prediction)
-svm_results = cbind(svm_test_all, svm_prediction_YN)
 
 svm_prediction_filter_lone_Y = apply(svm_results, 1, function(svm_result_row) {
 protein_id = svm_result_row['protein_id']
