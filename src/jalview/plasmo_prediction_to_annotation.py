503d44a5e7c5ec21eec3567b3d94bbb513daac03
Jurn Ho <jurn@magicmonster.com>
2016-08-22 23:21:08 +0100

    annotation motor domain

--- src/jalview/plasmo_prediction_to_annotation.py
+++ src/jalview/plasmo_prediction_to_annotation.py
@@ -37,11 +37,15 @@ def to_ranges(disordered_residue_positions):
 
 
 def main():
+    if len(sys.argv) != 3:
+        print "usage: " + sys.argv[0] + " [plasmodb_svm_results.csv] [motor_domains.csv]"
+        return
     csv_results_file = sys.argv[1]
+    motor_domains_file = sys.argv[2]
 
     # state held in here
     annotations = {}
-
+    # add disorder predictions
     with open(csv_results_file, 'rb') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
@@ -56,7 +60,7 @@ def main():
             protein_annotation = annotations[protein_id]
 
             distance = row['plasmo_svm_decision_values']
-            disorder = row['plasmo_svm.disorder']
+            disorder = row['plasmo_svm.disorder.smoothed']
             position = int(row['position'])
             if position != protein_annotation['previous_position'] + 1:
                 raise Exception("missing residue at position: " + str(protein_annotation['previous_position']))
@@ -64,7 +68,16 @@ def main():
             protein_annotation['distances'].append(distance)
             if disorder == 'Y':
                 protein_annotation["disordered_residue_positions"].append(position)
-
+    # add motor domain info
+    with open(motor_domains_file, 'rb') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            protein_id = row['protein id']
+            motor_domain_start = row['motor domain start']
+            motor_domain_end = row['motor domain end']
+            protein_annotation = annotations[protein_id]
+            protein_annotation['motor_domain_start'] = motor_domain_start
+            protein_annotation['motor_domain_end'] = motor_domain_end
     # dump annotations
     for protein_id, prediction_info in annotations.iteritems():
         print protein_id
@@ -83,10 +96,19 @@ def main():
                                       to + "\t*\n")
                 annotation_file.write(
                         "PROPERTIES\t" + group_name + "\tdescription=disordered region\t"
-                                                      "outlineColour=red\tcolour=yellow\n")
+                                                      "outlineColour=blue\tcolour=yellow\n")
             annotation_file.write("LINE_GRAPH\tSVM decision value\t" + "|".join(prediction_info["distances"]) + "\n")
             annotation_file.write("COLOUR\tSVM decision value\tblue\n")
-
+            if 'motor_domain_start' in prediction_info:
+                annotation_file.write('NO_GRAPH\tMotor Domain\t')
+                start = prediction_info['motor_domain_start']
+                # gaps, then H*
+                annotation_file.write("|" * (int(start) -1))
+                # move the label a bit further on.. jalview doesn't align it properlyy otherwise.
+                annotation_file.write("H|H|H|H|H|H,motor domain|")
+                end = prediction_info['motor_domain_end']
+                motor_domain_length = int(end) - int(start)
+                annotation_file.write("H|" * (motor_domain_length-6))
 
 if __name__ == "__main__":
     main()
